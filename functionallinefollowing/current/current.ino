c9d5a7d036a205e5809e72f21ffb11b85691ac6a
David Coffman <david.coffman@duke.edu>
2019-10-22 15:26:43 -0400

    Re-commented.

--- functionallinefollowing/current/current.ino
+++ functionallinefollowing/current/current.ino
@@ -16,25 +16,27 @@ void setup() {
 }
 
 void loop() {
-  // if left black, turn left
-  // if right black, turn right
-  // if all three, increment hashmarks
-
   checkSensors();
+
+  // For threshold determination.
   for (int i = 0; i < 3; i++) {
     Serial.print(QTIvalues[i]);
     Serial.print(" ");
   }
   Serial.println();
 
+  // Turn handling: right turn.
   if (QTIvalues[0] and not QTIvalues[2]) { // if left
     left.write(80); // left backwards
     right.write(80); // right forwards
   }
+  // Turn handling: left turn.
   else if (QTIvalues[2] and not QTIvalues[0]) {
     left.write(100); // left forwards
     right.write(100); // right backwards
   }
+  // Stop handling: reached a hashmark. 
+  // Still need to implement counting.
   else if (QTIvalues[0] and QTIvalues[1] and QTIvalues[2]) {
     left.detach();
     right.detach();
@@ -45,32 +47,29 @@ void loop() {
     right.write(85);
     delay(200);
   }
+  //
   else {
     left.write(100);
     right.write(85);
   }
 }
 
-/* 
- * INPUT:  Reads the QTIvalues from the QTI sensors stored in the array QTIpins[]
- * OUTPUT: A 0 or 1 in array QTIvalues[] depending on if the sensor reads a dark or light objects
- * In order to calibrate what is dark and what is light, use the threshold variable above
- * 
- * From: https://github.com/erisawesome/QTI-Sensor-Demo/blob/master/QTI-Sensor-Demo.ino
- */
+// Method for checking QTI sensors.
 void checkSensors() {
   for(int i = 0; i < sizeof(QTIpins)/2; i++) { 
-    long duration = 0;                      //
-    pinMode(QTIpins[i], OUTPUT);               // code that actually    
-    digitalWrite(QTIpins[i], HIGH);            // reads from the sensor 
-    delay(1);                               // 
-    pinMode(QTIpins[i], INPUT);                // low QTIvalues from the sensor
-    digitalWrite(QTIpins[i], LOW);             // are dark objects, high
-    while(digitalRead(QTIpins[i])){            // QTIvalues are light objects
-      duration++;                           //
-    }                                       
-    
-    QTIdurations[i] = duration;                // store QTIvalues in arrays
-    QTIvalues[i] = (duration > threshold);     // 
+    long duration = 0;                      
+    pinMode(QTIpins[i], OUTPUT);                 
+    digitalWrite(QTIpins[i], HIGH);           
+    delay(1);                               
+    pinMode(QTIpins[i], INPUT);              
+    digitalWrite(QTIpins[i], LOW);             
+    while(digitalRead(QTIpins[i])){
+      // Wait for RC circuit to decay.            
+      duration++;                          
+    }    
+                                       
+    // Convert read value to Boolean.
+    QTIdurations[i] = duration;               
+    QTIvalues[i] = (duration > threshold);     
   }
 }
