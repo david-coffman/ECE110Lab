8a52e65214437d596ba77a51409429b2edb5463c
David Coffman <david.coffman@duke.edu>
2019-10-23 20:19:12 -0400

    Added communication code.

---
+++ XbeeCommunication/XbeeCommunication.ino
@@ -0,0 +1,33 @@
+#define Rx 17
+#define Tx 16
+
+void setup() {
+  pinMode(45, OUTPUT);
+  pinMode(46, OUTPUT);
+ 
+  pinMode(4, INPUT);
+  Serial2.begin(9600);
+  Serial.begin(9600);
+  delay(500);
+  Serial.println("Init");
+}
+
+void loop() {
+  if(Serial2.available()){
+    char incoming = Serial2.read();
+    if( incoming == '5'){
+      digitalWrite(45, LOW);
+      Serial.println(incoming);
+      delay(500);
+    }
+  }
+  if(digitalRead(4)){
+    char outgoing = '5';
+    Serial2.write(outgoing);
+    digitalWrite(46,LOW);
+    Serial.println(outgoing);
+    delay(500);
+  }
+digitalWrite(45, HIGH);
+digitalWrite(46, HIGH);
+}
