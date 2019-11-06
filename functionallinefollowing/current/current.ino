8d51cc24a131fb76a0908a84474415a8ca70b8fe
David Coffman <david.coffman@duke.edu>
2019-11-06 16:47:26 -0500

    Added hash counting to line following code.

--- functionallinefollowing/current/current.ino
+++ functionallinefollowing/current/current.ino
@@ -8,14 +8,20 @@ int QTIdurations[3];
 int QTIvalues[3];
 
 int threshold = 100;
+int encountered = 0;
 
 void setup() {
   left.attach(11);
   right.attach(12);
   Serial.begin(9600);
+  Serial3.begin(9600);
 }
 
 void loop() {
+  Serial3.println();
+  Serial3.println();
+  Serial3.println("Hashes: "+String(encountered)+"       ");
+  Serial3.println("                ");
   checkSensors();
 
   // For threshold determination.
@@ -24,36 +30,42 @@ void loop() {
     Serial.print(" ");
   }
   Serial.println();
-
-  // Turn handling: right turn.
-  if (QTIvalues[0] and not QTIvalues[2]) { // if left
-    left.write(80); // left backwards
-    right.write(80); // right forwards
+  if( encountered < 5){
+    // Turn handling: right turn.
+    if (QTIvalues[0] and not QTIvalues[2]) { // if left
+      left.write(80); // left backwards
+      right.write(80); // right forwards
+    }
+    // Turn handling: left turn.
+    else if (QTIvalues[2] and not QTIvalues[0]) {
+      left.write(100); // left forwards
+      right.write(100); // right backwards
+    }
+    // Stop handling: reached a hashmark. 
+    // Still need to implement counting.
+    else if (QTIvalues[0] and QTIvalues[1] and QTIvalues[2]) {
+      left.detach();
+      right.detach();
+      delay(500);
+      encountered++;
+      encountered = encountered % 6;
+      left.attach(11);
+      right.attach(12);
+      left.write(100);
+      right.write(85);
+      delay(200);
+    }
+    //
+    else {
+      left.write(100);
+      right.write(85);
+    }
   }
-  // Turn handling: left turn.
-  else if (QTIvalues[2] and not QTIvalues[0]) {
-    left.write(100); // left forwards
-    right.write(100); // right backwards
-  }
-  // Stop handling: reached a hashmark. 
-  // Still need to implement counting.
-  else if (QTIvalues[0] and QTIvalues[1] and QTIvalues[2]) {
+  else{ 
     left.detach();
     right.detach();
-    delay(500);
-    left.attach(11);
-    right.attach(12);
-    left.write(100);
-    right.write(85);
-    delay(200);
-  }
-  //
-  else {
-    left.write(100);
-    right.write(85);
   }
 }
-
 // Method for checking QTI sensors.
 void checkSensors() {
   for(int i = 0; i < sizeof(QTIpins)/2; i++) { 
