7650e1ebc4d8ad9985cf7dc562ba4724283b545c
David Coffman <david.coffman@duke.edu>
2019-12-03 21:07:54 -0500

    Changed song.

--- IntegratedIDC/IntegratedIDC.ino
+++ IntegratedIDC/IntegratedIDC.ino
@@ -372,125 +372,60 @@ void lightShow() {
 
 // FINAL ROUTINE: SONG
 
-#define NOTE_B0  31
-#define NOTE_C1  33
-#define NOTE_CS1 35
-#define NOTE_D1  37
-#define NOTE_DS1 39
-#define NOTE_E1  41
-#define NOTE_F1  44
-#define NOTE_FS1 46
-#define NOTE_G1  49
-#define NOTE_GS1 52
-#define NOTE_A1  55
-#define NOTE_AS1 58
-#define NOTE_B1  62
-#define NOTE_C2  65
-#define NOTE_CS2 69
-#define NOTE_D2  73
-#define NOTE_DS2 78
-#define NOTE_E2  82
-#define NOTE_F2  87
-#define NOTE_FS2 93
-#define NOTE_G2  98
-#define NOTE_GS2 104
-#define NOTE_A2  110
-#define NOTE_AS2 117
-#define NOTE_B2  123
-#define NOTE_C3  131
-#define NOTE_CS3 139
-#define NOTE_D3  147
-#define NOTE_DS3 156
-#define NOTE_E3  165
-#define NOTE_F3  175
-#define NOTE_FS3 185
-#define NOTE_G3  196
-#define NOTE_GS3 208
-#define NOTE_A3  220
-#define NOTE_AS3 233
-#define NOTE_B3  247
-#define NOTE_C4  262
-#define NOTE_CS4 277
-#define NOTE_D4  294
-#define NOTE_DS4 311
-#define NOTE_E4  330
-#define NOTE_F4  349
-#define NOTE_FS4 370
-#define NOTE_G4  392
-#define NOTE_GS4 415
-#define NOTE_A4  440
-#define NOTE_AS4 466
-#define NOTE_B4  494
-#define NOTE_C5  523
-#define NOTE_CS5 554
-#define NOTE_D5  587
-#define NOTE_DS5 622
-#define NOTE_E5  659
-#define NOTE_F5  698
-#define NOTE_FS5 740
-#define NOTE_G5  784
-#define NOTE_GS5 831
-#define NOTE_A5  880
-#define NOTE_AS5 932
-#define NOTE_B5  988
-#define NOTE_C6  1047
-#define NOTE_CS6 1109
-#define NOTE_D6  1175
-#define NOTE_DS6 1245
-#define NOTE_E6  1319
-#define NOTE_F6  1397
-#define NOTE_FS6 1480
-#define NOTE_G6  1568
-#define NOTE_GS6 1661
-#define NOTE_A6  1760
-#define NOTE_AS6 1865
-#define NOTE_B6  1976
-#define NOTE_C7  2093
-#define NOTE_CS7 2217
-#define NOTE_D7  2349
-#define NOTE_DS7 2489
-#define NOTE_E7  2637
-#define NOTE_F7  2794
-#define NOTE_FS7 2960
-#define NOTE_G7  3136
-#define NOTE_GS7 3322
-#define NOTE_A7  3520
-#define NOTE_AS7 3729
-#define NOTE_B7  3951
-#define NOTE_C8  4186
-#define NOTE_CS8 4435
-#define NOTE_D8  4699
-#define NOTE_DS8 
-
-int tempo[]{
-  1.5,1.5,4,4,4,1.5,1.5,4,2,4,3,8,4,3,8,4,3,8,1.5,8,3,8,3,8,4,4,3,8,3,8,3,8,6,16,6,16,2,4,8,8,3,8,3,8,3,16,3,16,3,8,6,16,8,8,3,8,6,16,6,16,3,8
-  };
-
-int melody[]{
-  0,0,NOTE_AS2, NOTE_DS3, NOTE_B2, NOTE_AS3,0,NOTE_F3,NOTE_DS3,NOTE_AS3,NOTE_C4,NOTE_C4,NOTE_AS3,NOTE_A3,NOTE_A3,NOTE_AS3,NOTE_C4,NOTE_C4,NOTE_F3,NOTE_F3,
-  NOTE_F2,NOTE_F2,NOTE_G2,NOTE_G2,NOTE_F2,NOTE_A2,NOTE_AS2,NOTE_AS2,NOTE_A2,NOTE_F2,NOTE_A2,NOTE_F2,NOTE_AS2,NOTE_AS2,NOTE_AS2,NOTE_A2,NOTE_A2,NOTE_AS2,NOTE_A2,NOTE_F3,NOTE_F3,NOTE_G3,
-  NOTE_G3,NOTE_F3,NOTE_A3,NOTE_A3,NOTE_AS3,NOTE_AS3,NOTE_F3,NOTE_A3,NOTE_F3,NOTE_C3,NOTE_F3,NOTE_A3,NOTE_F3,NOTE_AS3,NOTE_AS3,NOTE_AS3,NOTE_A3,NOTE_A3,NOTE_F3
-  };
+const int note_g = 198;
+const int note_b = 245;
+const int note_c = 261;
+const int note_d = 294;
+const int note_e = 329;
+const int note_f = 349;
+const int note_gh = 391;
 
 void configureSong() {
   pinMode(8, OUTPUT);
 }
 
-void song() {
-  // put your main code here, to run repeatedly:
- for (int thisNote = 0; thisNote < sizeof(melody); thisNote++) {
+void loop(){
+  beep(note_c, 750);  
+  beep(note_c, 250);  
+  beep(note_b, 250);  
+  beep(note_c, 1000);
+ 
+  beep(note_c, 250);  
+  beep(note_b, 250);  
+  beep(note_c, 500);
+  
+  beep(note_d, 250);
+  beep(note_d, 500);
+  beep(note_f, 250);
+  beep(note_f, 750);
 
-      // to calculate the note duration, take one second
-      // divided by the note type.
-      //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
-      int noteDuration = 1000 / tempo[thisNote];
+  beep(note_e, 250);
+  beep(note_c, 250); //end of line 1
+  beep(note_d, 500);
+  beep(note_b, 250);
+  beep(note_g, 500);
+  beep(note_e, 250);
+  beep(note_c, 250);
+  beep(note_d, 750);
 
-      tone(8, melody[thisNote], noteDuration);
-
-      // to distinguish the notes, set a minimum time between them.
-      // the note's duration + 30% seems to work well:
-      int pauseBetweenNotes = noteDuration * 1.30;
-      delay(pauseBetweenNotes);
-
-  }
+  beep(note_gh, 250);
+  beep(note_c, 250);
+  beep(note_f, 500);
+  beep(note_e, 250);
+  beep(note_e, 750);
+  
+  beep(note_d, 250);
+  beep(note_d, 750);
+  
+  beep(note_c, 250);
+  beep(note_b, 250); //end of line 2
+  
+  delay(500);
+  delay(1000);
+}
+ 
+void beep(int note, int duration){
+  tone(buzzerPin, note, duration);
+  noTone(buzzerPin);
+  delay(50);
 }
